cf7d3869ceaac4c44d4ea6f9b6a6dfaa4630649c
Hironobu-okubo <ee171068oh@gmail.com>
2022-04-13 04:56:42 +0900

    passlist

---
+++ Python_Algo_BohyohShibata/chap02/pass_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 13 04:49:49 2022
+
+@author: okubo
+"""
+
+def change(lst, idx, val):
+    lst[idx] = val
+
+x = [11,22,33,44,55]
+print(x)
+index = int(input('インデックス：'))
+value = int(input('新しい順：'))
+change(x,index,value)
+print(x)

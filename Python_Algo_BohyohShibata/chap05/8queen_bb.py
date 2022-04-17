ba6efc4ad9f3935a1bdd7d242a5a0178afdc4dbf
Hironobu-okubo <ee171068oh@gmail.com>
2022-04-17 22:48:22 +0900

    8queen_bb

---
+++ Python_Algo_BohyohShibata/chap05/8queen_bb.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 17 21:51:19 2022
+
+@author: okubo
+"""
+pos = [0] * 3
+flag = [False] * 3
+
+def put():
+    for i in range(3):
+        print(f'{pos[i]}',end = ' ')
+    print()
+
+def set(i: int):
+    for j in range(3):
+        if not flag[j]:
+            pos[i] = j
+            if i ==  2:
+                put()
+            else:
+                flag[j] = True
+                set(i + 1)
+                flag[j] = False
+
+set(0)

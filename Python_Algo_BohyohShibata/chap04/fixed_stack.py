1f4ffda2614f9f771a9f17031f6a45dc226f6420
Hironobu-okubo <ee171068oh@gmail.com>
2022-04-22 23:36:50 +0900

    ｀add exceptionclass

---
+++ Python_Algo_BohyohShibata/chap04/fixed_stack.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 23:23:38 2022
+
+@author: okubo
+"""
+
+class Fixedstack:
+    class Empty:
+        pass
+    
+    class Full:
+        pass
+    
+    

fbafdb7dc7ca95544b2c1e8e6437976e5fa5672e
Hironobu-okubo <ee171068oh@gmail.com>
2022-04-23 21:43:38 +0900

    ｀add init

--- Python_Algo_BohyohShibata/chap04/fixed_stack.py
+++ Python_Algo_BohyohShibata/chap04/fixed_stack.py
@@ -7,10 +7,18 @@ Created on Fri Apr 22 23:23:38 2022
 """
 
 class Fixedstack:
+    """空のFixedStackに対してpopが呼び出された時"""
     class Empty:
         pass
     
+    """満杯のFixedStackにたいしてpushが呼び出された時"""
     class Full:
         pass
+    
+    def __init__(self,capacity):
+       self.stk = [None] * capacity
+       self.capacity = capacity
+       ptr = 0
+       
     
     
